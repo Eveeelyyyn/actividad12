fedcd190b5673539b110cf1e50e2fe70a3c4d36f
Eveeelyyyn <evsolacopernico@hotmail.com>
2022-11-03 00:07:20 -0600

    loading

---
+++ ejercicio02.js
@@ -0,0 +1,23 @@
+let imagenes = document.querySelectorAll(".pintura");
+
+for (let i = 0; i < imagenes.length; i++) {
+  imagenes[i].onmouseover = function() {
+
+    let newNode = document.createElement("img");
+    const parts=this.src.split("images");
+
+    newNode.src="images" + parts[parts.length-1];
+    newNode.className='imagezoom';
+
+    this.parentNode.appendChild(newNode);
+  }
+
+  imagenes[i].onmouseout = function(){
+
+    imagenes = document.querySelectorAll(".imagezoom");
+
+    for (let i=0; i < imagenes.length; i++){
+      imagenes[i].parentNode.removeChild(imagenes[i]);
+    }
+  }
+}
